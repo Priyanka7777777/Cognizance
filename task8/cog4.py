0d973f629dbba5c9364fb42fe98281312d397eb2
priyanka7777777 <sarafpriyanka434@gmail.com>
2022-03-20 19:53:09 +0545

    first commit

--- task8/cog4.py
+++ task8/cog4.py
@@ -1,3 +1,10 @@
-a= ("amrita school of engeering chennai campus")
-result = a.title()
-print(result)
+import pandas as pd
+result=""
+length= int(input("enter the length of array: "))
+element=[(input('enter the element: '))
+for i in range(length)]
+s_element= pd.Series(element)
+for i in range(len(element)):
+    result+=('' + s_element[i])
+    
+print(result.title())
